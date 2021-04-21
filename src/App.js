923fa1076e99441ae03b76573a1af5d49f9aa911
Manuel Congosto <mc@y2kwebs.com>
2021-04-21 23:53:06 +0200

    Exercici3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import data from "./data/frases.json";
 import styled from "styled-components";
+import React, { useState } from "react";
 
 const StyledDiv = styled.div`
-  display: flex;
-  flex-direction: column;
+  display: ${(props) => (props.display ? "flex" : "none")};
   width: 80%;
-  font-familily: Sans;
   padding: 10px;
 `;
 
 const StyledButton = styled.button`
-  width: 100%;
+  width: 50%;
+  padding: 30px;
 `;
 function App() {
+  const [index, setIndex] = useState(0);
+
   return (
-    <StyledDiv>
-      {data.map((e) => (
-        <StyledDiv>{e}</StyledDiv>
+    <div>
+      <StyledButton
+        disabled={index < 1}
+        onClick={() => setIndex((currentIndex) => currentIndex - 1)}
+      >
+        Anterior
+      </StyledButton>
+      <StyledButton
+        disabled={index >= data.length - 1}
+        onClick={() => setIndex((currentIndex) => currentIndex + 1)}
+      >
+        Siguiente
+      </StyledButton>
+      {data.map((e, indexData) => (
+        <StyledDiv display={indexData === index}>{e}</StyledDiv>
       ))}
-    </StyledDiv>
+    </div>
   );
 }
 
